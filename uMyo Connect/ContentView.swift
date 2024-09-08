e84a53fdaab465b9358e50c0dc7012870556b6d1
Aahil Lakhani <lakhaniaahil@gmail.com>
2024-09-08 16:03:07 -0400

    Initial Commit

---
+++ uMyo Connect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uMyo Connect
+//
+//  Created by Aahil Lakhani on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uMyo Connect/uMyo_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  uMyo_ConnectApp.swift
+//  uMyo Connect
+//
+//  Created by Aahil Lakhani on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct uMyo_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
